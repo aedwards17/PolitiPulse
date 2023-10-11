b3f458fc66fda15a58a75032603e07af6ef0fb71
aedwards17 <42212134+aedwards17@users.noreply.github.com>
2023-10-11 15:10:45 +0000

    NavBar Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
+import NavBar from './components/NavBar'
 
 export default function App() {
   return (
     <main>
-      React ⚛️ + Vite ⚡ + Replit 🌀
+      <NavBar />
     </main>
   )
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,20 @@
+export default function NavBar() {
+  return (
+    <nav className="nav">
+    <ul>
+      <li>
+          <a href="/home">Home</a>
+      </li>
+      <li>
+          <a href="/House">House</a>
+      </li>
+      <li>
+          <a href="/Senate">Senate</a>
+      </li>
+      <li>
+          <a href="/Misc">Misc</a>
+      </li>
+    </ul>
+  </nav>
+  )
+}
