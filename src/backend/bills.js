6b01339ffb8885faf14be3500403e4a9b62643a7
adamB6 <107077379+adamB6@users.noreply.github.com>
2023-10-16 17:58:40 +0000

    bill aggregation is now fully functional

--- src/backend/bills.js
+++ src/backend/bills.js
@@ -1,65 +1,90 @@
+// Import necessary modules
 const axios = require('axios');
 const admin = require('firebase-admin');
-const fbKey = process.env['FB_KEY'];
-const serviceAccount = fbKey;
 
+// Retrieve the Firebase service account key from an environment variable
+const serviceAccount = process.env['FB_KEY'];
+
+// Initialize the Firebase Admin SDK with the service account key and a database URL
+admin.initializeApp({
+  credential: admin.credential.cert(JSON.parse(serviceAccount)),
+  databaseURL: "https://politipulse-default-rtdb.firebaseio.com"
+});
+
+// Set up a connection to the Firestore database and configure Firestore settings
+const db = admin.firestore();
+db.settings({ ignoreUndefinedProperties: true });
+
+// Retrieve the API key for ProPublica from an environment variable
 const apiKey = process.env['API_KEY'];
 const apiUrl = 'https://api.propublica.org/congress/v1';
 
-const congress = 117; // 105-117
-const chamber = 'both'; // house, senate, or both
-const type = 'active'; // introduced, updated, active, passed, enacted, or vetoed
+// Define parameters for the ProPublica API request
+const congress = 117;
+const chamber = 'both';
+const type = 'active';
 
+// Define an asynchronous function to fetch and push data to Firestore
+async function fetchAndPushData() {
+  try {
+    // Make an HTTP request to the ProPublica API to fetch bill data
+    // "?offset=value" can be added to the end of the URL to page through the results
+    const response = await axios.get(`${apiUrl}/${congress}/${chamber}/bills/${type}.json`, {
+      headers: {
+        'X-API-Key': apiKey,
+      },
+    });
 
-axios.get(`${apiUrl}/${congress}/${chamber}/bills/${type}.json`, { // an offset can be placed here for expanding the database
-  headers: {
-    'X-API-Key': apiKey,
-  },
-})
-  .then(response => {
-    // Handle the API data here
     const data = response.data;
-    displayData(data);
-  })
-  .catch(error => {
+    if (data.results) {
+      const bills = data.results[0].bills;
+
+      // Loop through the fetched bills and store them in Firestore
+      for (const bill of bills) {
+        // Fetch the bill URL using another function
+        const bill_url = await getBillUrl(bill.bill_id, bill.bill_uri);
+
+        // Get a reference to the Firestore document and set its data
+        const docRef = db.collection('bills').doc(bill.bill_id);
+        await docRef.set({
+          bill_number: bill.number,
+          bill_title: bill.short_title,
+          bill_description: bill.title,
+          bill_date: bill.introduced_date,
+          bill_status: bill.active ? "Active" : "Not Active",
+          latest_major_action: bill.latest_major_action,
+          latest_major_action_date: bill.latest_major_action_date,
+          bill_summary: bill.summary,
+          bill_short_summary: bill.short_summary,
+          bill_url: bill_url,
+        });
+      }
+    } else {
+      console.log('No results found for the query.');
+    }
+  } catch (error) {
     console.error('Error fetching data:', error.message);
-  });
-
-function getBill(bill_id, url) {
-  axios.get(`${url}`, {
-    headers: {
-      'X-API-Key': apiKey,
-    },
-  })
-    .then(response => {
-      // Handle the API data here
-      const data = response.data.results[0].versions[0].url;
-      console.log(bill_id + " URL=" + data); //bill url will have to be inserted into firebase at this point
-    })
-    .catch(error => {
-      console.error('Error fetching data:', error.message);
-    });
-}
-
-function displayData(data) {
-  if (data.results) {
-    const bills = data.results[0].bills;
-
-    bills.forEach(bill => {
-      console.log("bill ID: " + bill.bill_id);
-      console.log("bill number: " + bill.number);
-      console.log("bill title: " + bill.short_title);
-      console.log("bill description: " + bill.title);
-      console.log("bill date: " + bill.introduced_date);
-      console.log("bill status: " + (bill.active ? "Active" : "Not Active"));
-      console.log("latest major action: " + bill.latest_major_action);
-      console.log("latest major action date: " + bill.latest_major_action_date);
-      console.log("bill summary: " + bill.summary);
-      console.log("bill short summary: " + bill.short_summary); //this can be improved with gpt4all in future
-      console.log(getBill(bill.bill_id, bill.bill_uri)) //this function will have to update firebase directly for the URL
-      console.log('--------------------------------------');
-    });
-  } else {
-    console.log('No results found for the query.');
   }
 }
+
+// Define an asynchronous function to fetch the bill URL
+async function getBillUrl(bill_id, url) {
+  try {
+    // Make an HTTP request to the ProPublica API to get the bill URL
+    const response = await axios.get(url, {
+      headers: {
+        'X-API-Key': apiKey,
+      },
+    });
+
+    // Extract the URL from the response data and log it
+    const data = response.data.results[0].versions[0].url;
+    return data;
+  } catch (error) {
+    console.error('Error fetching data:', error.message);
+    return null;
+  }
+}
+
+// Call the main function to start the data fetching and storing process
+fetchAndPushData();
