cac2c5ebca3171ac43f5f3f52201492eee176dab
aedwards17 <42212134+aedwards17@users.noreply.github.com>
2023-10-18 17:22:35 +0000

    Commented NavBar, and started onf Register

--- src/frontend/components/NavBar.jsx
+++ src/frontend/components/NavBar.jsx
@@ -1,18 +1,21 @@
+// Import necessary components and hooks from react-router-dom
 import {Link, useMatch, useResolvedPath} from "react-router-dom"
 
+// NavBar component for the top of the pages
 export default function NavBar() {
   return (
-    <nav className="nav">
+    <nav className="nav"> {/* Start of navigation bar */}
       <ul>
-        <Clink to="/">Home</Clink>
+        <Clink to="/">Home</Clink> {/* Link to Home page */}
         <li>
-            <div className="dropdown">
-              <a href="/House">House</a>
-              <div className="dropdown-content">
+            <div className="dropdown"> {/* Dropdown menu for House related links */}
+              <Clink to="/House">House</Clink> 
+              <div className="dropdown-content"> {/* Container for dropdown items */}
+                {/* Links for various House-related pages */}
                 <Clink to="/HCE">Current Elected</Clink>
-                <Clink to="/HE">Elction</Clink>
+                <Clink to="/HE">Election</Clink> 
                 <Clink to="/HRB">House Recent Business</Clink>
-                <Clink to="/HUB">House Upcoming Business </Clink>
+                <Clink to="/HUB">House Upcoming Business</Clink>
               </div>  
             </div>
         </li>
@@ -20,10 +23,11 @@ export default function NavBar() {
             <div className="dropdown">
               <a href="/Senate">Senate</a>
               <div className="dropdown-content">
+                {/* Links for various Senate-related pages */}
                 <Clink to="/SCE">Current Elected</Clink>
-                <Clink to="/SE">Elction</Clink>
+                <Clink to="/SE">Election</Clink> 
                 <Clink to="/SRB">Senate Recent Business</Clink>
-                <Clink to="/SUB">Senate Upcoming Business </Clink>
+                <Clink to="/SUB">Senate Upcoming Business</Clink>
               </div>  
             </div>
         </li>
@@ -31,6 +35,7 @@ export default function NavBar() {
             <div className="dropdown">
               <a href="/User">User</a>
               <div className="dropdown-content">
+                {/* Links for user-related pages */}
                 <Clink to="/UserProfile">Profile</Clink>
                 <Clink to="/About">About</Clink>
                 <Clink to="/Login">Login</Clink>
@@ -43,14 +48,18 @@ export default function NavBar() {
   )
 }
 
+// Custom link component that adds active class to the link that matches the current URL
 function Clink({to, children, ...props}) {
-  const resolvedPath = useResolvedPath(to)
-  const isActive = useMatch({path: resolvedPath.pathname, end: true})
+  // Resolve the path to an absolute path
+  const resolvedPath = useResolvedPath(to);
+  // Check if the current path matches the target path exactly
+  const isActive = useMatch({path: resolvedPath.pathname, end: true});
 
+  // Return a list item with the link. If the link is active, add the "active" class
   return (
     <li className={isActive ? "active" : ""}>
       <Link to={to} {...props}>
-        {children}
+        {children} {/* Render the link text or child components */}
       </Link>
     </li>  
   )
