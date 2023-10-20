b8d971e8b9fe427a7a67d21097756246f591ae20
aedwards17 <42212134+aedwards17@users.noreply.github.com>
2023-10-20 16:03:44 +0000

    Made Headway into Register functionality and CSS

--- src/frontend/pages/user/Register.jsx
+++ src/frontend/pages/user/Register.jsx
@@ -1,38 +1,35 @@
 import React from 'react'
+import {useForm} from 'react-hook-form'
 
 export default function Register() {
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  
+  } = useForm()
+
+  const onSubmit = data => {
+    console.log(data);
+  }
+
   return (
-    <NameForm />
-  )
-}
-
-class NameForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {value: ''};
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-
-  handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.value);
-    event.preventDefault();
-  }
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Name:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
-        </label>
-        <input type="submit" value="Submit" />
+    <div className="Reg">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className='form-control'>
+          <label>Email</label>
+          <input type="text" name="email" {...register("email")} />
+        </div>
+        <div className='form-control'>
+          <label>Password</label>
+          <input type="password" name="password" {...register("password")} />
+        </div>
+        <div className='form-control'>
+          <label></label>
+          <button type="submit">Login</button>
+        </div>
       </form>
-    );
-  }
+    </div>
+  )
 }
