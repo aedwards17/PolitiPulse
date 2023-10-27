ac56dcdd9666daf905e8f120ed58ccbac814bfcb
asbotens <10203411-asbotens@users.noreply.replit.com>
2023-10-27 12:26:52 +0000

    gitignore..

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,13 +1,1 @@
-// src/firebase.js
 
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-
-const serviceAccount = process.env('FB_API_KEY')
-const firebaseConfig = {serviceAccount};
-
-// Initialize Firebase
-const app = firebase.initializeApp(firebaseConfig);
-const db = app.firestore();
-// test 4
-export { db };
