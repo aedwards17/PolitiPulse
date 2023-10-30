2e2738bc885780b6089b588fe4d118f1f69c25bc
aedwards17 <42212134+aedwards17@users.noreply.github.com>
2023-10-30 18:07:39 +0000

    Login Page UI/Functionality

--- src/frontend/pages/user/Login.jsx
+++ src/frontend/pages/user/Login.jsx
@@ -1,35 +1,74 @@
-import React from 'react'
-import {useForm} from 'react-hook-form'
+import React, {useRef, useState} from 'react'
+import { Card, Form, Button, Container, Alert } from 'react-bootstrap'
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
-export default function Register() {
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors }
 
-  } = useForm()
-
-  const onSubmit = data => {
-    console.log(data);
-  }
-
+export default function Login(){
   return (
-    <div className="Reg">
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className='form-control'>
-          <label>Email</label>
-          <input type="text" name="email" {...register("email")} />
-        </div>
-        <div className='form-control'>
-          <label>Password</label>
-          <input type="password" name="password" {...register("password")} />
-        </div>
-        <div className='form-control'>
-          <label></label>
-          <button type="submit">Login</button>
-        </div>
-      </form>
-    </div>
+    <Container 
+      className = "d-flex align-items-center justify-content-center" 
+      style={{minHeight:"100vh"}}
+    >
+      <div className="w-100" style={{maxWidth:"400px"}}>
+        <LoginComp />
+      </div>
+    </Container>
+  )
+}
+
+
+
+function LoginComp() {
+  const emailRef = useRef()
+  const passwordRef = useRef()
+
+  const auth = getAuth();
+
+  function onSubmit(e) {
+    e.preventDefault(); // Prevents the default form submit action
+
+    const auth = getAuth();
+    signInWithEmailAndPassword(
+      auth, 
+      emailRef.current.value, 
+      passwordRef.current.value
+    )
+      .then((userCredential) => {
+        
+        const user = userCredential.user;
+        console.log(user)
+        console.log("User is logged in")
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.error(errorCode, errorMessage);
+      });
+    
+  }
+  return (
+    <>
+      <Card>
+        <Card.Body>
+          <h2 className="text-center mb-4">Login</h2>
+          <Form onSubmit={onSubmit}>
+            <Form.Group id="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" ref={emailRef} required />
+            </Form.Group>
+            <Form.Group id="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" ref={passwordRef} required />
+            </Form.Group>
+            <Button className="w-100" type="submit">
+              Login
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+      <div className="w-100 text-center mt-2">
+      </div>
+    </>
   )
 }
